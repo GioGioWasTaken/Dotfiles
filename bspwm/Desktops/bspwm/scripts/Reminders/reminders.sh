51bf1321e7d70b6f9ac948bf0a6245da8c48d674
Iyar, Dad's house <iyar.segev@gmail.com>
2024-08-13 18:16:03 +0300

    remidner script finalized

--- bspwm/Desktops/bspwm/scripts/Reminders/reminders.sh
+++ bspwm/Desktops/bspwm/scripts/Reminders/reminders.sh
@@ -19,25 +19,24 @@ current_minute=$(date +%M)
 
 # Check for specific times for lunch reminder
 if [[ "$current_time" == "14:00" || "$current_time" == "15:00" || "$current_time" == "16:00" || "$current_time" == "17:00" ]]; then
-    send_reminder "Time to eat lunch!" "Steak.png" "low" 5000
+    send_reminder "Time to eat lunch!" "Steak.png" "low" 7000
 fi
 
 # Check every 20 minutes for hydration reminder
 if (( $(date +%M) % 20 == 0 )); then
-    send_reminder "Stay Hydrated!" "Water_bottle.png" "normal" 1000
+    send_reminder "Stay Hydrated!" "Water_bottle.png" "normal" 3000
 fi
 
 # Check if current time is within the range for hourly Anki reminders
 if (( 7 <= current_hour && current_hour < 21 &&current_minute==0 || current_minute==30)); then
-    send_reminder "Bi-Hourly Anki reminder!" "Anki.png" "low" 3000
+    send_reminder "Bi-Hourly Anki reminder!" "Anki.png" "low" 5000
 fi
 
 
 if ((current_hour =21 &&current_minute==0 )); then
-    send_reminder "Stop working. ご苦労さまでした" "Bed.png" "low" 3000
+    send_reminder "Stop working. ご苦労さまでした" "Bed.png" "low" 8000
 fi
 
 
 
 
-send_reminder "Stay Hydrated!" "Water_bottle.png" "normal" 1000
