f00d0e9277bc4551c524d8461fd959a70e3f1708
Iyar Desktop Mom <iyar.segev@gmail.com>
2025-05-26 13:33:31 +0300

    obsidian udapted note generation method

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -5,4 +5,16 @@ require("obsidian").setup({
       path = "$PROGRAMMING_DIR/Brain/",
     },
   },
+
+
+
+-- Optional, customize how note file names are generated given the ID, target directory, and title.
+  ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+  ---@return string|obsidian.Path The full path to the new note.
+  note_path_func = function(spec)
+    -- This is equivalent to the default behavior.
+    local title = spec.title or spec.id
+    local path = spec.dir / title
+    return path:with_suffix(".md")
+  end,
 })
