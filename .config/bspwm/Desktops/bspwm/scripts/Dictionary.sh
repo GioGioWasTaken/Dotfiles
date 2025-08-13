00101dfeaa80b421bcecf9097df0ed8ef3d1d2d2
Iyar Desktop Mom <iyar.segev@gmail.com>
2025-08-13 14:33:10 +0300

    remove desktops/

--- .config/bspwm/Desktops/bspwm/scripts/Dictionary.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Open Rofi and get user input
-word=$(echo "" | rofi -dmenu -p "Enter a Japanese word:")
-
-# Call lookup_word() function and display result
-output=$(python ~/.local/bin/dictionary.py "$word")
-echo "$output" | rofi -dmenu -p "Result:"
