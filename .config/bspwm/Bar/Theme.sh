3f33a9c1e0fd4d2c7178aff8b40923686385de3e
Iyar Desktop Mom <iyar.segev@gmail.com>
2026-03-24 09:31:03 +0200

    switch bar for something more minimalstic. bspwm code is now fully mine (except eww widgets)

--- .config/bspwm/Bar/Theme.sh
+++ .config/bspwm/Bar/Theme.sh
@@ -1,97 +1,22 @@
 #!/usr/bin/env bash
-#  ██████╗  █████╗ ███╗   ███╗███████╗██╗      █████╗     ██████╗ ██╗ ██████╗███████╗
-#  ██╔══██╗██╔══██╗████╗ ████║██╔════╝██║     ██╔══██╗    ██╔══██╗██║██╔════╝██╔════╝
-#  ██████╔╝███████║██╔████╔██║█████╗  ██║     ███████║    ██████╔╝██║██║     █████╗
-#  ██╔═══╝ ██╔══██║██║╚██╔╝██║██╔══╝  ██║     ██╔══██║    ██╔══██╗██║██║     ██╔══╝
-#  ██║     ██║  ██║██║ ╚═╝ ██║███████╗███████╗██║  ██║    ██║  ██║██║╚██████╗███████╗
-#  ╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝ ╚═════╝╚══════╝
-#  Author  :  z0mbi3
-#  Url     :  https://github.com/gh0stzk/dotfiles
-#  
-# This is one of the only files I will keep from the original rice. All credit of course goes to z0mbi3.
-#
 
 set_bspwm_config() {
-    bspc config border_width 0
-    bspc config top_padding 59
-    bspc config bottom_padding 2
-    bspc config left_padding 2
-    bspc config right_padding 2
-    bspc config normal_border_color "#C574DD"
-    bspc config active_border_color "#C574DD"
-    bspc config focused_border_color "#8897F4"
-    bspc config presel_feedback_color "#FF4971"
-}
-
-# Reload terminal colors
-set_term_config() {
-    cat > "$HOME"/.config/alacritty/rice-colors.toml << EOF
-
-# Default colors
-[colors.primary]
-background = "#1D1F28"
-foreground = "#FDFDFD"
-
-# Cursor colors
-[colors.cursor]
-cursor = "#FF4971"
-text = "#1D1F28"
-
-# Normal colors
-[colors.normal]
-black = "#3D4C5F"
-blue = "#8897F4"
-cyan = "#79E6F3"
-green = "#5ADECD"
-magenta = "#C574DD"
-red = "#F37F97"
-white = "#FDFDFD"
-yellow = "#F2A272"
-
-# Bright colors
-[colors.bright]
-black = "#56687E"
-blue = "#556FFF"
-cyan = "#3FDCEE"
-green = "#18E3C8"
-magenta = "#B043D1"
-red = "#FF4971"
-white = "#BEBEC1"
-yellow = "#FF8037"
-EOF
+	bspc config border_width 0
+	bspc config top_padding 0
+	bspc config bottom_padding 0
+	bspc config left_padding 0
+	bspc config right_padding 0
+	bspc config normal_border_color "#C574DD"
+	bspc config active_border_color "#C574DD"
+	bspc config focused_border_color "#8897F4"
+	bspc config presel_feedback_color "#FF4971"
 }
 
 # Set dunst notification daemon config
-set_dunst_config() {
-    sed -i "$HOME"/.config/bspwm/dunstrc \
-        -e "s/transparency = .*/transparency = 9/g" \
-        -e "s/frame_color = .*/frame_color = \"#1D1F28\"/g" \
-        -e "s/separator_color = .*/separator_color = \"#8897F4\"/g" \
-        -e "s/font = .*/font = JetBrainsMono NF Medium 9/g" \
-        -e "s/foreground='.*'/foreground='#79E6F3'/g"
-
-    sed -i '/urgency_low/Q' "$HOME"/.config/bspwm/dunstrc
-    cat >> "$HOME"/.config/bspwm/dunstrc <<- _EOF_
-			[urgency_low]
-			timeout = 3
-			background = "#1D1F28"
-			foreground = "#FDFDFD"
-
-			[urgency_normal]
-			timeout = 6
-			background = "#1D1F28"
-			foreground = "#FDFDFD"
-
-			[urgency_critical]
-			timeout = 0
-			background = "#1D1F28"
-			foreground = "#FDFDFD"
-_EOF_
-}
 
 # Set eww colors
 set_eww_colors() {
-    cat > "$HOME"/.config/bspwm/eww/colors.scss << EOF
+	cat >"$HOME"/.config/bspwm/eww/colors.scss <<EOF
 // Eww colors 
 \$bg: #1D1F28;
 \$bg-alt: #1F222B;
@@ -108,28 +33,22 @@ set_eww_colors() {
 EOF
 }
 
-
 POLYBAR="$XDG_CONFIG_HOME/bspwm/Bar"
 # Launch the bar
 launch_bars() {
 
-    for mon in $(polybar --list-monitors | cut -d":" -f1); do
-        (MONITOR=$mon polybar -q pam1 -c "$POLYBAR"/config.ini)&
-        (MONITOR=$mon polybar -q pam2 -c "$POLYBAR"/config.ini)&
-        (MONITOR=$mon polybar -q pam3 -c "$POLYBAR"/config.ini)&
-        (MONITOR=$mon polybar -q pam4 -c "$POLYBAR"/config.ini)&
-        (MONITOR=$mon polybar -q pam5 -c "$POLYBAR"/config.ini)&
-        (MONITOR=$mon polybar -q pam6 -c "$POLYBAR"/config.ini)&
-    done
+	# This launches polybar on multiple monitors
+	for mon in $(polybar --list-monitors | cut -d":" -f1); do
+		(MONITOR=$mon polybar -c "$XDG_CONFIG_HOME/bspwm/Bar/config.ini") &
+		# (MONITOR=$mon polybar -q pam2 -c "$POLYBAR"/config.ini) &
+		# (MONITOR=$mon polybar -q pam3 -c "$POLYBAR"/config.ini) &
+		# We can launch multiple bars per monitor in this way, but I am only interested in having 1 bar. No longer interested in "rice switching". The only thing I change from time to time is wallpaper.
+	done
 
 }
-
-
 
 ### ---------- Apply Configurations ---------- ###
 
 set_bspwm_config
-set_term_config
 launch_bars
-set_dunst_config
 set_eww_colors
